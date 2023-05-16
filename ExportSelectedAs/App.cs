484c24ebbfcbf4e24cbefcd610ca7616be2df2a9
JakkeLab <itsjakk3@gmail.com>
2023-05-16 20:41:54 +0900

    first commit

---
+++ ExportSelectedAs/App.cs
@@ -0,0 +1,117 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using ExportSelectedAs.TabUI;
+using System;
+using System.Reflection;
+
+namespace ExportSelectedAs
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try
+            {
+                // Create a new Ribbon Tab
+                string tabName = "Export As";
+                application.CreateRibbonTab(tabName);
+                // assembly
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                // assembly path
+                string assemblyPath = assembly.Location;
+
+                // Create a new Ribbon Panel
+                RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Export Group");
+
+                // Create a new Push Button
+                PushButtonData buttonData = new PushButtonData("Export Groups as NWC", "Export\nGroup(.nwc)", assemblyPath, "ExportSelectedAs.Command");
+
+                // Add the Push Button to the Ribbon Panel
+                PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+                //Create Show Button
+                PushButtonData showButtonData = new PushButtonData("Show Window", "Show", assemblyPath, "ExportSelectedAs.Show");
+                PushButton showButton = ribbonPanel.AddItem(showButtonData) as PushButton;
+
+                //register dockablepane
+                RegisterDockablePane(application);
+
+                // return result
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                return Result.Failed;
+            }
+        }
+
+
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result RegisterDockablePane(UIControlledApplication application)
+        {
+            // dockablepaneviewer (customcontrol)
+            MyDockablePane window = new MyDockablePane();
+
+            // register in application with a new guid
+            DockablePaneId dockID = new DockablePaneId(new Guid("{2ab6b447-1be8-4439-bbd1-c3e9d4037d64}"));
+            try
+            {
+                application.RegisterDockablePane(dockID, "Dock Pane",
+                    window as IDockablePaneProvider);
+
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error Message", ex.Message);
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+    }
+
+    public class CommandAvailability : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication app, CategorySet cate)
+        {
+            if (app.ActiveUIDocument == null)
+            {
+                //disable register btn
+                return true;
+            }
+            //enable register btn
+            return false;
+        }
+    }
+    // external command class
+    [Transaction(TransactionMode.Manual)]
+    public class Show : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                // dockable window id
+                DockablePaneId id = new DockablePaneId(new Guid("{2ab6b447-1be8-4439-bbd1-c3e9d4037d64}"));
+                DockablePane dockableWindow = commandData.Application.GetDockablePane(id);
+                dockableWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                // show error info dialog
+                TaskDialog.Show("Info Message", ex.Message);
+            }
+            // return result
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ExportSelectedAs/Command.cs
@@ -0,0 +1,136 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
+
+namespace ExportSelectedAs
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiApp = commandData.Application;
+            var uiDoc = uiApp.ActiveUIDocument;
+            var doc = uiDoc.Document;
+
+            //선택모드
+            IList<Reference> pickedRefs;
+            try
+            {
+                pickedRefs = uiDoc.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element, new ModelGroupSelectionFilter(), "모델그룹 선택");
+            }
+            catch (Exception ex)
+            {
+                return Result.Cancelled;
+            }
+
+            if (pickedRefs == null)
+                return Result.Cancelled;
+
+            //내보내기
+            FolderBrowserDialog fldDlg = new FolderBrowserDialog();
+            DialogResult result = fldDlg.ShowDialog();
+            if (result != DialogResult.OK || string.IsNullOrEmpty(fldDlg.SelectedPath))
+                return Result.Cancelled;
+
+            //파일 있는지 한번 검사
+            string folderPath = fldDlg.SelectedPath;
+            var fileNames = Directory.GetFiles(folderPath).ToList();
+            List<string> fileNamesToExport = new List<string>();
+            foreach (Reference pickedRef in pickedRefs)
+            {
+                Element modelGroup = doc.GetElement(pickedRef.ElementId);
+                List<ElementId> elementIds = GetElementsInModelGroup(doc, modelGroup as Group);
+                fileNamesToExport.Add(Path.Combine(modelGroup.Name + ".nwc"));
+            }
+
+            //이미 있는경우 
+            if(fileNames.Count(x => fileNamesToExport.Contains(x)) != 0)
+            {
+                DialogResult msgResult = MessageBox.Show("일부 파일이 이미 폴더에 있습니다. 계속하시겠습니까?", "파일 덮어쓰기", MessageBoxButtons.YesNo);
+                if(msgResult == DialogResult.No)
+                {
+                    return Result.Cancelled;
+                }
+            }
+
+            foreach (Reference pickedRef in pickedRefs)
+            {
+                Element modelGroup = doc.GetElement(pickedRef.ElementId);
+                List<ElementId> elementIds = GetElementsInModelGroup(doc, modelGroup as Group);
+                string filePath = Path.Combine(fldDlg.SelectedPath, modelGroup.Name + ".nwc");
+                if(File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+                ExportToNWC(doc, elementIds, fldDlg.SelectedPath, modelGroup.Name);
+            }
+            return Result.Succeeded;
+
+        }
+
+        private List<ElementId> GetElementsInModelGroup(Document doc, Group modelGroup)
+        {
+            List<ElementId> elementIds = new List<ElementId>();
+
+            // 모델 그룹에 포함된 Element들 가져오기
+            ICollection<ElementId> modelGroupMemberIds = modelGroup.GetMemberIds();
+
+            foreach (ElementId memberId in modelGroupMemberIds)
+            {
+                Element member = doc.GetElement(memberId);
+                if (member != null)
+                    elementIds.Add(memberId);
+            }
+
+            return elementIds;
+        }
+
+        private string GetElementName(ElementId elemId, Document doc)
+        {
+            return doc.GetElement(elemId).Name;
+        }
+
+        private void ExportToNWC(Document doc, ICollection<ElementId> elementIds, string outputPath, string fileName)
+        {
+            var option = new NavisworksExportOptions();
+            option.SetSelectedElementIds(elementIds);
+            option.ExportScope = NavisworksExportScope.SelectedElements;
+            try
+            {
+                doc.Export(outputPath, fileName, option);
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Error", ex.Message, TaskDialogCommonButtons.Ok);
+                return;
+            }
+        }
+    }
+
+    public class ModelGroupSelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Group;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+
+
+}
